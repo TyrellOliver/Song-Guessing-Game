143894006fcb6d5b8ef1a100ecd32aa50c559f3f
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-03-03 15:00:25 -0500

    feat(api): add root endpoint

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ index: "Welcome to the Song Guessing Game" });
+});
+
 module.exports = app;
